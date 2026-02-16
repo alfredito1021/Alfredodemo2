5b83d8d4416a97ea0545565f44840a3796ab7a01
alfredito1021 <aroch125@my.epcc.edu>
2026-02-16 21:29:07 +0000

    banana and apple

--- Alfredo.java
+++ Alfredo.java
@@ -1,9 +1,29 @@
+import java.util.Scanner;
+
 public class Alfredo
 {
-public static void main (String[] args)
-{
-    System.out.println("It works!");
-    System.out.println("yeah!!!!");
-}
+    public static void main (String[] args)
+    {
+            Scanner keyboard = new Scanner(System.in);
 
+            System.out.println("Enter secret code: ");
+            String code = keyboard.nextLine();
+
+            if(code.equals("banana"))
+            {
+                System.out.println("Thank you!");
+            }
+     else
+        
+            if(code.equals("apple"))
+        {
+                System.out.println("Thank you!");
+        }
+    else 
+    {
+        System.out.println("Error");
+    }
+    
 }
+}
+        
